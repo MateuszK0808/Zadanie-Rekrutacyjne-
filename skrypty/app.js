838d67a082288d956c6ce6b93800a79e50457a22
MateuszK0808 <mateuszk0808@interia.pl>
2017-01-30 11:47:34 +0100

    dodano strzałki do layoutu dla mobile

--- skrypty/app.js
+++ skrypty/app.js
@@ -1,10 +1,13 @@
 var myApp = angular.module('myApp',[]);
 
 myApp.controller('myController',['$scope','$http',function($scope,$http) {
-  $scope.wynik = {};
-  $scope.wynik.rata = '2';
-  $scope.wynik.szkoda = '';
+  $scope.wynik = {
+    rata: '2',
+    szkoda: '',
+    suma: 5050
+  };
   $scope.zaznaczone = '';
+
   $scope.unselect = function() {
 
     if ($scope.wynik.szkoda === $scope.zaznaczone && $scope.wynik.szkoda !== '') {
